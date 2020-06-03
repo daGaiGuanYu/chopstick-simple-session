e79bc82832e371d1a7960fba0ea219c514b3e020
关于 <372301467@qq.com>
2020-06-03 11:17:29 +0800

    update

--- test/index.js
+++ test/index.js
@@ -1,3 +1,15 @@
-const Cookie = require('cookie')
+const { Server, Handler } = require('chopstick')
+const SSession = require('../src/index')
 
-console.log(Cookie.parse(''))
+Server.start(8974)
+
+Handler.add('GET', '/', ctx => {
+  SSession.put(ctx.res, {
+    name: 1,
+    id: 3
+  }, {
+    tel: 2,
+    haha: 'weixianfa'
+  })
+  return 1
+})
